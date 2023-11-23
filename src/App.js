3451ffed0b3660de83c73538bc24ca1506349d8f
monil <user@Monil-MacBook.local>
2023-11-22 21:12:09 -0500

    added

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HomePage from "./component/feature/homepage/container/homePage.container";
+import DealerSignup from "./component/feature/dealerSignup/container/dealerSignup.container";
+import DealerCustomization from "./component/feature/dealerSignup/components/dealerCustomization";
+import DealerAgreement from "./component/feature/dealerSignup/components/dealerAgreement";
+import StartSelling from "./component/feature/dealerSignup/container/startSelling.container";
+import HealthCheck from "./component/feature/homepage/components/HealthCheck";
+import Header from "./component/common/header";
+import Footer from "./component/common/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <div className="flex justify-center">
+          <Routes>
+            <Route exact path="/home" element={<HomePage />} />
+            <Route exact path="/dealer-signup" element={<DealerSignup />} />
+            <Route exact path="/hcheckup" element={<HealthCheck />} />
+            <Route
+              exact
+              path="/dealer-customization"
+              element={<DealerCustomization />}
+            />
+            <Route
+              exact
+              path="/dealer-agreement"
+              element={<DealerAgreement />}
+            />
+            <Route
+              exact
+              path="/dealer-logo-and-location"
+              element={<StartSelling />}
+            />
+            <Route path="*" element={<HomePage />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/feature/homepage/components/HealthCheck.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HealthCheck = () => {
+    return (
+        <h3>Hello</h3>
+    );
+}
+
+export default HealthCheck;
